b9e843f433279484c8041faca1c12c1544dd2ad9
SameeraDayaratne <85645373+SameeraDayaratne@users.noreply.github.com>
2023-10-07 01:28:49 +0530

    Add project files.

---
+++ BooksEcommerceWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BooksEcommerceWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

da02297e2eb998bd186ec2fca517d3edcdfe8268
SameeraDayaratne <85645373+SameeraDayaratne@users.noreply.github.com>
2023-10-08 02:13:44 +0530

    Added Category

---
+++ BooksEcommerceWeb/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using BooksEcommerceWeb.Data;
+using BooksEcommerceWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BooksEcommerceWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> categoryList = _db.Categories.ToList();
+            return View(categoryList);
+        }
+    }
+}

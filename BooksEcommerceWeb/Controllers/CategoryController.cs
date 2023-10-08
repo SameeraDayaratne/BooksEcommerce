3740dc88425f7d4b90b75b5c18d84622e393d96c
SameeraDayaratne <85645373+SameeraDayaratne@users.noreply.github.com>
2023-10-08 15:12:16 +0530

    Implemented Edit Delete functionality of Category

--- BooksEcommerceWeb/Controllers/CategoryController.cs
+++ BooksEcommerceWeb/Controllers/CategoryController.cs
@@ -34,10 +34,73 @@ namespace BooksEcommerceWeb.Controllers
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category Created Successfully";
                 return RedirectToAction("Index");
             }
-            return View();  
-            
+            return View();
+
         }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0 ) 
+            {
+                return NotFound();
+            }
+
+            Category categoryFromDb = _db.Categories.Find(id);
+
+            if (categoryFromDb == null) 
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+           
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category Updated Successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Category categoryFromDb = _db.Categories.Find(id);
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Category categoryObj = _db.Categories.Find(id);
+            if (categoryObj == null) 
+            {
+                return NotFound();
+            }
+            
+            _db.Categories.Remove(categoryObj);
+            _db.SaveChanges();
+            TempData["success"] = "Category Deleted Successfully";
+            return RedirectToAction("Index");
+                     
+        }
+
     }
 }

ab8a1fdc400a26957177164e37ea7862e9eccf4d
SameeraDayaratne <85645373+SameeraDayaratne@users.noreply.github.com>
2023-10-14 00:40:17 +0530

    Added Product Model and created Product Repository

---
+++ BooksEcommece.DataAccess/Repository/IRepository/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using BooksEcommece.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooksEcommece.DataAccess.Repository.IRepository
+{
+    public interface IProductRepository : IRepository<Product>
+    {
+        void SaveChanges();
+        void Update(Product product);
+    }
+}
---
+++ BooksEcommece.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,30 @@
+﻿using BooksEcommece.DataAccess.Repository.IRepository;
+using BooksEcommece.Models.Models;
+using BooksEcommerceWeb.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooksEcommece.DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ProductRepository(ApplicationDbContext db) : base(db) 
+        {
+            _db = db;    
+        }
+        public void SaveChanges()
+        {
+            _db.SaveChanges();
+        }
+
+        public void Update(Product product)
+        {
+            _db.Products.Update(product);
+        }
+    }
+}

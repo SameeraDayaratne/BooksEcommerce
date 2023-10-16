9d4680b9a87d34b3a434540b748ce256c7f64c44
SameeraDayaratne <85645373+SameeraDayaratne@users.noreply.github.com>
2023-10-16 23:30:25 +0530

    Added Identity

--- BooksEcommece.DataAccess/Data/ApplicationDbContext.cs
+++ BooksEcommece.DataAccess/Data/ApplicationDbContext.cs
@@ -1,11 +1,14 @@
 ﻿
 using BooksEcommece.Models.Models;
 using BooksEcommerceWeb.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+
 
 namespace BooksEcommerceWeb.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
@@ -17,6 +20,8 @@ namespace BooksEcommerceWeb.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
+
             modelBuilder.Entity<Category>().HasData(
                 new Category {Id = 1 , Name = "Action" , DisplayOrder = 1 },
                 new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
--- BooksEcommerceWeb/Program.cs
+++ BooksEcommerceWeb/Program.cs
@@ -2,6 +2,7 @@ global using BooksEcommerceWeb.Data;
 global using Microsoft.EntityFrameworkCore;
 using BooksEcommece.DataAccess.Repository;
 using BooksEcommece.DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +11,8 @@ builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options=>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnction")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 builder.Services.AddScoped<IProductRepository, ProductRepository>();

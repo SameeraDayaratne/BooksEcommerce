0b7fe98d96eba27fdde210ca7bfcac5823da7ecc
SameeraDayaratne <85645373+SameeraDayaratne@users.noreply.github.com>
2023-10-17 19:44:26 +0530

    Added a new Application user

--- BooksEcommece.DataAccess/Data/ApplicationDbContext.cs
+++ BooksEcommece.DataAccess/Data/ApplicationDbContext.cs
@@ -18,6 +18,8 @@ namespace BooksEcommerceWeb.Data
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
 
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
--- BooksEcommerceWeb/Program.cs
+++ BooksEcommerceWeb/Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options=>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnction")));
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
-
+builder.Services.AddRazorPages();
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
@@ -32,8 +32,9 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
-
+app.MapRazorPages();
 app.MapControllerRoute(
     name: "default",
     pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
